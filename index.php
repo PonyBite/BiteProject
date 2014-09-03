5ccab2fa405f67ad581267a5d8d985828d2b2ad8
PonyBite <pony@yopmail.com>
2014-09-03 22:06:44 +0200

    c'est mocheeeeeee
    Lel

--- index.php
+++ index.php
@@ -2,16 +2,27 @@
 <html>
 <head>
 <title>DICKS</title>
- 
+ <link rel=STYLESHEET href="css/fstyles.css" type="text/css">
 </head>
 
 <body>
 <?php
 
 	echo "
-	<div class='bite'>
-		THIS GONNA CUM!
-	</div>
+	
+		<section class='pos-page'>
+			<nav class='bite'>
+				<a href='/html/'>acceuil</a> |
+				<a href='/css/'>MEC</a> |
+				<a href='/js/'>MEUF</a> |
+				<a href='/jquery/'>Se Connecter</a>
+			</nav>
+			<p class='titleNice'>Bienvenue sur notre site :)</p>
+			<section 'separateurRose'>
+				caca
+			</section>
+			
+		</section>
 	";
 /*GOOD LUCK AND HAVE FUN LITTLE PONY ;) ;D*/
 ?>
